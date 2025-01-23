850632a2d7b2f4ee8f50d0ee0380059ff54de567
Ricardo Rodriguez <5296200+uy-rrodriguez@users.noreply.github.com>
2025-01-23 18:05:41 +0100

    Updated `functions.sh` to display node information

--- functions.sh
+++ functions.sh
@@ -48,3 +48,51 @@ run_with_time_track() {
     echo
     echo "End $(date +'%d/%m/%Y %H:%M:%S') (${HOURS}h ${MINS}m ${SECS}s)"
 }
+
+
+run_with_emissions_track() {
+    # Wrap execution of any command with a set of logging information to
+    # calculate CO2 emissions.
+    #
+    # Examples:
+    #   run_with_emissions_track echo "Test"
+    #   run_with_emissions_track \
+    #       echo \
+    #           "Test" \
+    #           2>&1 \
+    #           | tee test.txt
+    #
+
+    function display_info() {
+        # Display information from scontrol
+        JOB_FIELDS="ArrayTaskId JobName RunTime StartTime EndTime NodeList \
+                    NumNodes MinCPUsNode MinMemoryNode Features"
+        GREP_REGEX="JobId"
+        for f in $JOB_FIELDS; do
+            GREP_REGEX="$GREP_REGEX|$f"
+        done
+        GREP_REGEX="(^| )($GREP_REGEX)=[^\s]*"
+
+        echo
+        echo "Job information:"
+        echo "────────────────"
+        scontrol show job $SLURM_JOB_ID | grep -Po "$GREP_REGEX" | grep -Po "\w.*"
+        echo "────────────────"
+    }
+
+    trap display_info INT TERM
+
+
+    # Node allocated, necessary to then calculate CO2 emissions
+    # TODO: Add support for multiple nodes
+    NODE=$(scontrol show job $SLURM_JOB_ID 2>/dev/null \
+           | grep -Po " NodeList=[^\s]+" | grep -Po "=.+" | grep -Po "\w+")
+    GPU=$(sinfo -o "%N %G" | grep "${NODE}" | grep -Po "gpu:.+" \
+          | grep -Po ":.+?:" | grep -Po "[^:]+")
+    echo "Allocated nodes: ${NODE} (${GPU})"
+    echo
+
+    $@
+
+    display_info
+}
