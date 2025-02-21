4ff94599441e4701b021498fe65ebf2d1f0ff048
uy-rrodriguez <5296200+uy-rrodriguez@users.noreply.github.com>
2025-02-21 09:38:30 +0100

    Added log P for each letter, and HF perplexity

--- util/llm_scores.py
+++ util/llm_scores.py
@@ -14,7 +14,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 # Trick to import local packages when this script is run from the terminal
 sys.path.append(os.path.abspath("."))
 
-from deft import get_prompt, template_from_id
+from deft import get_prompt, template_from_id, linearize_instance
 
 # HuggingFace authentication
 # from util.hugging_face import hf_login
@@ -383,19 +383,22 @@ def calc_sample_logp(
     """
     Calculates the log probability for a given sentence.
 
-    Log Probability is defined as:
-      LogP(S) = Sum( log P(wi|w1, w2, ..., wi-1) )
+    Given S a sequence of k possible answers w_1 w_2 ... w_k, we want to
+    calculate P(w_1...w_k EOS|prompt) where w_1 ... w_k are the possible answers
+    (a, b, ..., ab, ac, ..., abcde).
 
-    where wi is the i-th token in the sequence S.
+    Log Probability is then defined as:
 
-    This can be interpreted as the probability given by the model to a specific
-    sequence of tokens.
+        log P(w_1...w_k EOS|prompt) =
+            log(P(EOS|prompt, w_1 ... w_k))
+            + P(w_k|prompt, w_1 ... w_{k-1})
+            + ... + log(P(w_1|prompt))
+
+    This can be interpreted as the probability given by the model to the
+    sequence of tokens of a given answer.
 
     https://blog.uptrain.ai/decoding-perplexity-and-its-significance-in-llms/
     """
-    def answer_to_str(letter):
-        return f'({letter}) {inst["answers"][letter]}'
-
     log(f"{'-'*80}\n{inst['id']}")
 
     base_prompt: str = get_prompt(
@@ -403,44 +406,134 @@ def calc_sample_logp(
         inst,
         add_left_parenthesis=False,
     )
+    # Remove last space from base prompt (after "Réponse(s) :")
+    # This space will be added as part of the answer, otherwise the first letter
+    # is tokenized as "(a" while the rest are tokenized as single characters,
+    # e.g. "b".
+    base_prompt = base_prompt[:-1]
+
+    # Get logits from the model
+    base_inputs = tokenizer(base_prompt, return_tensors="pt")
+    len_prompt = base_inputs.input_ids.size(1) - 1  # Ignore BOS
 
     results = {}
     for comb in combinations:
         log(f"COMBINATION: {' '.join(comb)}")
 
+        letters = list(inst["answers"].keys())
+        eos = tokenizer.eos_token
+
         # Get logits from the model
-        prompt = (
-            f"{base_prompt}"
-            f"{'; '.join(answer_to_str(c) for c in comb)}"
-            f".\n{tokenizer.eos_token}"
+        _, answer = linearize_instance(
+            inst, included_answers=comb,
+            include_full_answers=True,
+            add_left_parenthesis=False,
         )
-        inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+        # Add space removed from base prompt and EOS
+        answer = " " + answer + eos
+        answer_inputs = tokenizer(answer, return_tensors="pt")
 
+        # Join tokens of base prompt and answers
+        input_ids = torch.concat(
+            (base_inputs.input_ids[0], answer_inputs.input_ids[0, 1:]),
+        ).unsqueeze(0).to("cuda")
+
+        # "Manually" get logits from model based on sequence
         with torch.no_grad():
-            outputs = model(inputs.input_ids)
+            outputs = model(input_ids)
         # outputs.logits.shape => (batch, seq length, vocab size)
-        input_ids = inputs.input_ids.squeeze(0)  # Remove batch dimension
-        log(input_ids)
-        log(input_ids.shape)
-        logits = outputs.logits.squeeze(0)
-        log(logits)
-        log(logits.shape)
 
-        log_probs = []
+        # Our algorithm differs from model.compute_transition_scores:
+        # - compute_transition_scores returns the score of each token in a
+        #   sequence at each generation step.
+        # - compute_transition_scores expects output scores as returned by
+        #   model.generate, i.e.: Tuple of len generated sequence, where each
+        #   Tensor is of shape (batch size, vocab size).
+        # - It's not useful for us because we need the logits of all the tokens
+        #   at each generation step, so we can calculate softmax over them.
+        #
+        # transition_scores = model.compute_transition_scores(
+        #     input_ids, [t for t in outputs.logits.reshape(input_ids.size(1), 1, -1)],
+        # )
+        # log(transition_scores)
+        # log(transition_scores.shape)
+
+        # Remove batch dimension
+        input_ids = input_ids.squeeze(0)
+        logits: torch.Tensor = outputs.logits.squeeze(0)
+
+        log_probs = torch.tensor([
+            torch.log(
+                # softmax of logits at pos i-1
+                #
+                # Note: I assume that at each position i, logits[i] contains the
+                # scores for each token in the vocabulary given the token i.
+                # So, the score for the token x_i knowing x_j<i is found at
+                # logits[i-1].
+                torch.softmax(logits[i-1], dim=-1)
+                # token id
+                [input_ids[i]]
+            )
+            for i in range(1, input_ids.size(0))  # ignore first token BOS
+        ])
+
+        log_probs_letters = {}
         for i in range(1, input_ids.size(0)):
-            prob = torch.softmax(logits[i-1], dim=-1)
+            prob = log_probs[i-1].item()
             token_id = input_ids[i]
-            log_prob = torch.log(prob[token_id])
-            log_probs.append(log_prob.item())
+            token = tokenizer.decode(token_id)
+
+            # Search probability of answer letters
+            found = False
+            if i >= len_prompt and token_id != tokenizer.eos_token_id:
+                token_ = token.strip()
+                for t in letters:
+                    _prev = tokenizer.decode(input_ids[i-1])
+                    _next = tokenizer.decode(input_ids[i+1])
+                    prev_is_par = _prev.endswith("(")
+                    next_is_par = _next.startswith(")")
+                    found = (
+                        token_ == t and prev_is_par and next_is_par
+                        or token_ == f"({t}" and next_is_par
+                        or token_ == f"{t})" and prev_is_par
+                        or token_ == f"({t})"
+                    )
+                    if found:
+                        # log(tokenizer.decode(token_id) + "  <==")
+                        log_probs_letters[t] = prob
+                        letters.remove(t)
+                        break
+
+            if found:
+                log(f"{token} ({token_id}) => {prob}  <<<")
+            else:
+                log(f"{token} ({token_id}) => {prob}")
+
+        # Make sure we found all letters in the current combination
+        assert list(comb) == sorted(log_probs_letters), (
+            "Not all letters in the current combination were captured."
+            f" Expected: {comb}. Found {log_probs_letters}."
+        )
 
         # Sequence probability
-        seq_log_prob = sum(log_probs)
-        log(f"Sequence log prob: {seq_log_prob}")
-        log(f"EOS log prob: {log_probs[-1]}")
+        prompt_logp = log_probs[:len_prompt].sum().item()
+        seq_logp = log_probs[len_prompt:].sum().item()
+        eos_logp = log_probs[-1].item()
+        letters_logp = sum(log_probs_letters.values()) + eos_logp
+        log(f"Sequence log P: {seq_logp}")
+        log(f"Prompt log P: {prompt_logp}")
+        log(f"EOS log P: {eos_logp}")
+        log(f"Letters + EOS log P: {letters_logp}")
+        log("\n" + "*"*80 + "\n")
 
         results[" ".join(comb)] = {
-            "seq_log_prob": seq_log_prob,
-            "eos_log_prob": log_probs[-1],
+            "seq_logp": seq_logp,
+            "prompt_logp": prompt_logp,
+            "eos_logp": eos_logp,
+            "letters_logp": letters_logp,
+            "letters": log_probs_letters,
+            "seq_len": input_ids.size(0) - len_prompt,
+            "prompt_len": len_prompt,
         }
 
     return results
@@ -452,24 +545,25 @@ def calc_model_logp(
         tokenizer: AutoTokenizer = None,
         corpus_path: str = "data/test-medshake-score.json",
         output_path: str =
-            "output/model_scores/llama3/llama-3-8b-deft_002_20240731-logp.json",
+            "output/model_scores/llama3/llama-3-8b-deft_002_20240731-logp_20250218.json",
 ):
     """
     Queries the model with all possible combinations of answers and calculates
     the Negative Log Likelihood of each sentence.
     """
     if model is not None and tokenizer is not None:
-        log(f"Using model '{model.__class__.__name__}'")
+        log(f"Using model '{model.name_or_path}'")
     else:
         log(f"Using model '{model_path}'")
         model, tokenizer = load_model(model_path)
 
-    print(f"Loading corpus '{corpus_path}")
+    log(f"Loading corpus '{corpus_path}")
     with open(corpus_path) as fp:
         corpus = json.load(fp)
 
     # For debugging
     # id = "4c0a40502de05e79aacd7131e714319e80300f37a119a944516fbde8e1d006c4"
+    # id = "c2dc3f530e04c26e52fb50f570f73bf6cdc804ff7b65a12d215049c82f13a90a"
     # corpus = filter(lambda s: s["id"] == id, corpus)
 
     choices = "a b c d e".split()
@@ -482,6 +576,138 @@ def calc_model_logp(
     for inst in corpus:
         sample_results = calc_sample_logp(model, tokenizer, inst, combs)
         all_results[inst["id"]] = sample_results
+
+    with open(output_path, "w") as fp:
+        json.dump(all_results, fp, indent=2)
+
+    return all_results
+
+
+def hf_perplexity(
+        model: AutoModelForCausalLM,
+        tokenizer: AutoTokenizer,
+        sequence: str,
+) -> torch.Tensor:
+    """
+    Perplexity calculated following HuggingFace's algorithm, based on the
+    entropy loss of a sequence.
+
+    https://huggingface.co/docs/transformers/perplexity
+    """
+    max_length = model.config.max_length
+    stride = max_length
+
+    encodings = tokenizer(sequence, return_tensors="pt")
+    seq_len = encodings.input_ids.size(1)
+
+    # Process the entire sequence at once (gives lower perplexity)
+    max_length = seq_len
+
+    log(f"max_length={max_length}, stride={stride}, seq_len={seq_len}")
+
+    nll_sum = 0.0
+    n_tokens = 0
+    prev_end_loc = 0
+    for begin_loc in range(0, seq_len, stride):
+        end_loc = min(begin_loc + max_length, seq_len)
+        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+        input_ids = encodings.input_ids[:, begin_loc:end_loc].to("cuda")
+        target_ids = input_ids.clone()
+        target_ids[:, :-trg_len] = -100
+
+        log(f"begin_loc={begin_loc}, end_loc={end_loc}, trg_len={trg_len}")
+        # log(f"{input_ids}, {target_ids}")
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+
+            # loss is calculated using CrossEntropyLoss which averages over
+            # valid labels
+            # N.B. the model only calculates loss over trg_len - 1 labels,
+            # because it internally shifts the labels to the left by 1.
+            neg_log_likelihood = outputs.loss
+
+            # NLL can be 0 if the length of input_ids is 1
+            if neg_log_likelihood.isnan():
+                neg_log_likelihood = torch.tensor(0)
+
+            log(f"neg_log_likelihood = {neg_log_likelihood}")
+
+        # Accumulate the total negative log-likelihood and the total number of
+        # tokens
+        num_valid_tokens = (target_ids != -100).sum().item()  # number of valid tokens in target_ids
+        batch_size = target_ids.size(0)
+        num_loss_tokens = num_valid_tokens - batch_size  # subtract batch_size due to internal label shift
+        nll_sum += neg_log_likelihood * num_loss_tokens
+        n_tokens += num_loss_tokens
+
+        # log(f"{num_valid_tokens} - {batch_size} = {num_loss_tokens}")
+
+        prev_end_loc = end_loc
+        if end_loc == seq_len:
+            break
+
+    avg_nll = nll_sum / n_tokens  # average negative log-likelihood per token
+    ppl = torch.exp(avg_nll)
+    log(f"exp({avg_nll}) = {ppl}")
+    return ppl
+
+
+def calc_hf_perplexity(
+        model_path: str = "models/llama3/llama-3-8b-deft_002_20240731",
+        model: AutoModelForCausalLM = None,
+        tokenizer: AutoTokenizer = None,
+        corpus_path: str = "data/test-medshake-score.json",
+        output_path: str =
+            "output/model_scores/llama3/llama-3-8b-deft_002_20240731-perp_20250219.json",
+):
+    """
+    Uses HuggingFace evaluate metric "perplexity" for each possible combination
+    of answers, for each sample in the corpus.
+    """
+    if model is not None and tokenizer is not None:
+        log(f"Using model '{model.name_or_path}'")
+    else:
+        log(f"Using model '{model_path}'")
+        model, tokenizer = load_model(model_path)
+
+    print(f"Loading corpus '{corpus_path}")
+    with open(corpus_path) as fp:
+        corpus = json.load(fp)
+
+    # For debugging
+    # id = "4c0a40502de05e79aacd7131e714319e80300f37a119a944516fbde8e1d006c4"
+    # id = "c2dc3f530e04c26e52fb50f570f73bf6cdc804ff7b65a12d215049c82f13a90a"
+    # corpus = filter(lambda s: s["id"] == id, corpus)
+
+    choices = "a b c d e".split()
+    combs = []
+    for i in range(1, len(choices) + 1):
+        combs.extend(itertools.combinations(choices, i))
+    combs.sort(key=len)
+
+    all_results = {}
+    for inst in corpus:
+        log("*" * 80)
+        log(inst["id"])
+        results = {}
+        base_prompt: str = get_prompt(
+            template_from_id("0"),
+            inst,
+            add_left_parenthesis=False,
+        )
+        for comb in combs:
+            log("-" * 80)
+            log(comb)
+            _, answer = linearize_instance(
+                inst, included_answers=comb,
+                include_full_answers=True,
+                add_left_parenthesis=False,
+            )
+            prompt = base_prompt + answer + tokenizer.eos_token
+            ppl = hf_perplexity(model, tokenizer, prompt)
+            results[" ".join(comb)] = ppl.item() if not ppl.isnan() else "-Inf"
+        all_results[inst["id"]] = results
 
     with open(output_path, "w") as fp:
         json.dump(all_results, fp, indent=2)
